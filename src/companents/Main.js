1fbdc842a5ef7424ddae3bc9e5a7e2ab4170b839
ahmet-sa <ahmeetsarioglu@gmail.com>
2022-12-06 21:46:27 +0300

    first commit

---
+++ src/companents/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Main(){
+
+    return (
+    
+    <main>
+        <h1 className="main--title">Fun Fact About React</h1>
+        <ul className="main--facts">
+            <li>İlk kez 2015'te yayınlandı</li>
+            <li>git-hub ta 180k yıldızın üzerinde </li>
+            <li>facebook tarafınfan geliştiriyor</li>
+            <li>binlerce kurumsal uygulamaya güç verin, mobil uygulamalar dahil</li>
+
+        </ul>
+
+    </main>
+    
+    
+    
+        )
+}
---
+++ src/companents/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar(){
+
+    return (
+    <nav>
+        <img src="../images/images.png" alt=""  />
+        <img src="../images/indir.png" alt="" />
+        <h3>React Gerçekleri </h3>
+        <h4>React kurs </h4>
+    
+
+    </nav>
+    )
+}
